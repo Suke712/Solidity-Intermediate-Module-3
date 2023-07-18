2f82d48d94668eeaf87df000b1392a8301cbbcb8
Suke712 <137681354+Suke712@users.noreply.github.com>
2023-07-18 10:28:58 +0530

    Create fallbackPayableChallenge.sol

---
+++ fallbackPayableChallenge.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.13;
+
+contract A{
+    function deposit() external payable {}
+
+    function () external payable {}
+
+}
+
+contract B{
+    address private addressOfA;
+
+    function callA(address _addressOfA) external payable{
+        (bool success, ) = _addressOfA.call(abi.encodeWithSignature("Withdraw()"));
+        require(success, "Callback function is not called");
+    }
+}
